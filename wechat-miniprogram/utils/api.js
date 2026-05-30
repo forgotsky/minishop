990187d836e915a7a83af9879f0976b5e7ef8b77
zhaoliuyang1988 <zhaoliuyang@zhaoliuyang.cn>
2026-05-31 02:28:18 +0800

    Update miniprogram BASE_URL to renewshuttle.cn

--- wechat-miniprogram/utils/api.js
+++ wechat-miniprogram/utils/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://127.0.0.1:8000'
+const BASE_URL = 'http://renewshuttle.cn'
 
 const request = (method, path, data, needAuth) => {
   return new Promise((resolve, reject) => {
